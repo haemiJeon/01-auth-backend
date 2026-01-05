58c41e3aa1773d2bb3db9defbc808a6c252617fc
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2026-01-05 15:37:51 +0900

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -24,12 +24,10 @@ export class AppController {
   async signUp(@Body() body: { email: string; password: string }) {
     const { email, password } = body;
 
-    // 비밀번호 암호화 (보안 강도: 10)
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // TODO: 이미 존재하는 이메일이면 에러
 
-    // Prisma로 DB에 데이터 저장
     const user = await this.prisma.user.create({
       data: {
         email,
@@ -47,7 +45,6 @@ export class AppController {
   async login(@Body() body: { email: string; password: string }) {
     const { email, password } = body;
 
-    // DB에서 정보 가져왔다고 가정
     const user = await this.prisma.user.findUnique({
       where: {
         email,
@@ -58,14 +55,12 @@ export class AppController {
       return { message: '존재하지 않는 이메일입니다.' };
     }
 
-    // 비밀번호 비교
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
       return { message: '비밀번호가 일치하지 않습니다.' };
     }
 
-    // 로그인 성공
     return {
       message: '로그인 성공',
       user: { id: user.id, email: user.email },
