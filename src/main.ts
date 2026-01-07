09305bcc64ea6e3109fb01e182c15c2fe4d8c0c1
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2026-01-07 14:55:38 +0900

    CORS 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    // 운영 환경(production)이면 특정 도메인만 허용, 개발 환경이면 모두 허용(true)
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? ['https://a.com', 'https://b.com']
+        : true,
+    credentials: true, // 쿠키/인증 헤더 전달 허용
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
