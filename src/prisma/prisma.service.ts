402576f7257bd549d0225e49973f8c21c17430b2
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2026-02-02 16:09:05 +0900

    chore: 환경별 Prisma Client 자동 생성 및 Service 설정 개선

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
@@ -6,6 +7,15 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor(private readonly config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
